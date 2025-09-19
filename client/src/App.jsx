54f35bb508c9ac2f697f28dfbdb9efb38224eaa2
elson2121 <elsonsd21@gmail.com>
2025-09-19 04:00:28 -0300

    feat: Add core project structure and initial styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import CarouselEffect from '../src/components/Carousel/CarouselEffect '
-import Header from './components/Header/Header'
+ import Router from '../Router'
 import './App.css'
-
+ 
+ 
 function App() {
   
   return (
-    <>
-    <Header/>
-    <CarouselEffect />
+    <> 
+  <Router/>
     </>
+    
   )
 }
 
